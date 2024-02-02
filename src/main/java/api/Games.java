2acef9201839caf507c1ce72bd0f2a8ad8ae1a55
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-02-02 03:04:40 -0300

    añadidios getters setters

--- src/main/java/api/Games.java
+++ src/main/java/api/Games.java
@@ -14,7 +14,42 @@ public class Games {
 
     public String nombre;
     public String fecha;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(String puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
     public String puntuacion;
+
+    @Override
+    public String toString() {
+        return "Games{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", puntuacion='" + puntuacion + '\'' +
+                '}';
+    }
 
     public Games() {
     }
